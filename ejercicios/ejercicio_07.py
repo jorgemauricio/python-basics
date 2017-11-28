7426bcecd6cb9fe5c7ac8cac5047d85ff1420a76
Jorge Mauricio <jorge.mauricio@me.com>
2017-11-28 15:48:58 -0600

    ejercicios 6 y 7

---
+++ ejercicios/ejercicio_07.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 17 16:17:25 2017
+
+@author: jorgemauricio
+
+Instrucciones
+
+- Cambiar el valor de dos variables sin utilizar una variable temporal
+
+""
