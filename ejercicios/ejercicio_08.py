7c95653dc5d7409be6dc8c2e5a3526be9f251b7a
Jorge Mauricio <jorge.mauricio@me.com>
2017-11-29 09:30:59 -0600

    correct grammar

--- ejercicios/ejercicio_08.py
+++ ejercicios/ejercicio_08.py
@@ -5,12 +5,6 @@ Created on Mon Jul 17 16:17:25 2017
 
 @author: jorgemauricio
 
-Instrucciones
-
-Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
-between 2000 and 3200 (both included).
-The numbers obtained should be printed in a comma-separated sequence on a single line.
-
 - 	Escribir un programa el cual encuentre los numeros divisibles entre 7 pero que no sean multiplos
 	de 5, entre los valores 1, 1000. Los numeros que se obtengan deben de imprimirse separados por una "," 
 	
