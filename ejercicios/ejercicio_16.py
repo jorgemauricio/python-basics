1810067938a37c81f8484cab916048bcae99b23b
Jorge Mauricio <jorge.mauricio@me.com>
2017-12-11 00:16:08 -0600

    agregar ejercicio 16

---
+++ ejercicios/ejercicio_16.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 17 16:17:25 2017
+
+@author: jorgemauricio
+
+Instrucciones
+	1. Elevar al cuadrado una lista dada por el usuario de 10 elementos,
+    se debe de mostrar la lista original y la lista elevada al cuadrado.
+    Ej.
+    a = [1,2,3,4,5,6,7,8,9,10]
+    R = [1,4,9,16,25,36,49,64,81,100]
+"""
